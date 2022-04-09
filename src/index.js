848ca864e1d21519055dd2c3397e14b20e125551
Sheena Gonzalez <shngonzalez@gmail.com>
2022-04-09 12:52:42 -0700

    added logger middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots } from './reducers';
 import 'tachyons';
 import App from './containers/App';
 
-const store = createStore(searchRobots)
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger))
 
 ReactDOM.createRoot(
   document.getElementById('root')
